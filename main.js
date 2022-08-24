5124622e1dbf53f38b91fc416e41cc02f0f7d863
Heeral-Coding <91391992+Heeral-Coding@users.noreply.github.com>
2022-08-24 17:11:42 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+
+noseX = 0;
+noseY = 0;
+
+function preload() {
+    lip = loadImage("https://i.postimg.cc/0QxQZ1Hg/lip-clipart-transparent-background-2.png");
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);  
+}
+
+function modelLoaded() {
+    console.log("poseNet is Initialized");
+}
+
+function gotPoses(results)
+
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y + 15;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(lip, noseX, noseY - 20, 45, 25);
+}
+
+function take_snapshot() {
+    save("heeral.png"); 
+}
